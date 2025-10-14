430d465169ea743b6609d26768a9ff54286b1b1a
JimmoLimmo <159819151+JimmoLimmo@users.noreply.github.com>
2025-10-14 14:43:10 -0500

    Started L4E1

--- lab4/code/Lab4EX1.cpp
+++ lab4/code/Lab4EX1.cpp
@@ -1,61 +1,70 @@
-//Use g++ -std=c++11 -o Lab4EX1 Lab4EX1.cpp -lwiringPi
+// Use g++ -std=c++11 -o Lab4EX1 Lab4EX1.cpp -lwiringPi
 
-#include <string>
-#include <iostream>
-#include <wiringSerial.h>
-#include <wiringPi.h>
 #include <unistd.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
 #include <cstdlib>
 #include <iomanip>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int kobuki;
 
-int main(){
-	wiringPiSetup();
-	kobuki = serialOpen("/dev/kobuki", 115200);
-	unsigned int bumper;
-	unsigned int drop;
-	unsigned int cliff;
-	unsigned int button;
-	unsigned int read;
+int main() {
+    wiringPiSetup();
+    kobuki = serialOpen("/dev/kobuki", 115200);
+    unsigned int bumper;
+    unsigned int drop;
+    unsigned int cliff;
+    unsigned int button;
+    unsigned int read;
 
-	while(serialDataAvail(kobuki) != -1){
+    while (serialDataAvail(kobuki) != -1) {
+        while (true) {
+            // If the bytes are a 1 followed by 15, then we are
+            // parsing the basic sensor data packet
+            read = serialGetchar(kobuki);
+            if (read == 1) {
+                if (serialGetchar(kobuki) == 15)
+                    break;
+            }
+        }
 
-		while(true){
-		//If the bytes are a 1 followed by 15, then we are
-		//parsing the basic sensor data packet
-			read = serialGetchar(kobuki);
-			if(read == 1){
-				if(serialGetchar(kobuki) == 15) break;
-			}
-		}
+        // Read past the timestamp
+        serialGetchar(kobuki);
+        serialGetchar(kobuki);
 
-		//Read past the timestamp
-		serialGetchar(kobuki);
-		serialGetchar(kobuki);
+        /*Read the bytes containing the bumper, wheel drop,
+                and cliff sensors. You can convert them into a usable data type.*/
+        bumper = serialGetchar(kobuki);
+        drop = serialGetchar(kobuki);
+        cliff = serialGetchar(kobuki);
 
-		/*Read the bytes containing the bumper, wheel drop,
-			and cliff sensors. You can convert them into a usable data type.*/
+        /*Print the data to the screen.*/
+        cout << "Bumper: " << bumper << "  Drop: " << drop << "  Cliff: " << cliff << endl;
+        /*Read through 6 bytes between the cliff sensors and
+        the button sensors.*/
+        for (int i = 0; i < 6; i++)
+            serialGetchar(kobuki);
 
-		/*Print the data to the screen.*/
+        /*Read the byte containing the button data.*/
+        button = serialGetchar(kobuki);
+        cout << "Button: " << button << endl;
 
-		/*Read through 6 bytes between the cliff sensors and
-		the button sensors.*/
+        /*Close the script and the connection to the Kobuki when
+        Button 1 on the Kobuki is pressed. Use serialClose(kobuki);*/
+        if (button & 0x02) {
+            cout << "Button 1 pressed. Closing connection..." << endl;
+            serialClose(kobuki);
+            break;
+        }
 
-		/*Read the byte containing the button data.*/
+        // Pause the script so the data read receive rate is the same as the Kobuki send rate.
+        usleep(20000);
+    }
 
-		/*Close the script and the connection to the Kobuki when
-		Button 1 on the Kobuki is pressed. Use serialClose(kobuki);*/
-
-
-		
-
-		//Pause the script so the data read receive rate is the same as the Kobuki send rate.
-		usleep(20000);
-			
-		
-	}
-
-	return(0);
+    return (0);
 }
+github
