a7926dc745e187c3e342d66f34937d448be39cef
Jimmy <jtn2642@gmail.com>
2025-10-28 16:48:12 -0500

    Edited Parse Data

--- lab5/code/static/parse_data.js
+++ lab5/code/static/parse_data.js
@@ -1,36 +1,49 @@
 if (!!window.EventSource) {
     var source = new EventSource('/');
     source.onmessage = function(e) {
-      var bumper = e.data[1]
-      var cliff = e.data[3];
-      var drop = e.data[5];
+      // var bumper = e.data[1]
+      // var cliff = e.data[3];
+      // var drop = e.data[5];
+    var bumper = parseInt(e.data[1]);
+    var cliff  = parseInt(e.data[3]);
+    var drop   = parseInt(e.data[5]);
 
 
       // finish the code to handle the bumper status
-      if (bumper == "0") {
-        document.getElementById("bumperStatus").value = "OFF";
-      }
-      if (bumper == "1") {
-        document.getElementById("bumperStatus").value = "ON";
+      if (bumper == 0) {
+        document.getElementById("bumperStatus").value = "No contact";
+      } else {
+        let status = [];
+        if (bumper & 0x01) status.push("Right");
+        if (bumper & 0x02) status.push("Center");
+        if (bumper & 0x04) status.push("Left");
+        document.getElementById("bumperStatus").value = status.join(" + ");
       }
          
         
-        // finish the code to handle the wheel drop status 
-      if (drop == "0") {
-        document.getElementById("wheelDropStatus").value = "OFF";
-      }
-      if (drop == "1") {
-        document.getElementById("wheelDropStatus").value = "ON";
+      // finish the code to handle the wheel drop status 
+      // 0x01 for right wheel, 0x02 for left wheel
+      if (drop == 0) {
+        document.getElementById("wheelDropStatus").value = "OK";
+      } else {
+        let dropStatus = [];
+        if (drop & 0x01) dropStatus.push("Right Dropped");
+        if (drop & 0x02) dropStatus.push("Left Dropped");
+        document.getElementById("wheelDropStatus").value = dropStatus.join(" + ");
       }
 
       // finish the code to handle cliff status 
-      if (cliff == "0") {
-        document.getElementById("cliffStatus").value = "OFF";
+      // 0x01 for right cliff, 0x02 for center cliff, 0x04 for left cliff
+      if (cliff == 0) {
+        document.getElementById("cliffStatus").value = "Clear";
+      } else {
+        let cliffStatus = [];
+        if (cliff & 0x01) cliffStatus.push("Right");
+        if (cliff & 0x02) cliffStatus.push("Center");
+        if (cliff & 0x04) cliffStatus.push("Left");
+        document.getElementById("cliffStatus").value = cliffStatus.join(" + ");
       }
-      if (cliff == "1") {
-        document.getElementById("cliffStatus").value = "ON";
-      }
-        
+          
 
 
     }
